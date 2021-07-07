8fdf0d07ebf5c8b2ad153b64fb5d7083fca44fb5
GLOBAL T <GLOBAL T@GLOBALT-PC>
2021-07-07 08:30:03 +0530

    wh-3421-Done Demo

--- src/main/java/com/example/demo/test/HelloRunner.java
+++ src/main/java/com/example/demo/test/HelloRunner.java
@@ -8,6 +8,7 @@ public class HelloRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Second Demo Git");
+		System.out.println("I done this Demo Git");
 	}
 
 }
